6ad4aad80659e0521d78c9c52b4f22c281b6fe42
arezoo <93465403+arezoomohammadi22@users.noreply.github.com>
2024-12-11 12:59:58 +0330

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import MessageDashboard from "./components/MessageDashboard";
+
+const App = () => (
+    <div>
+        <Login />
+        <Register />
+        <MessageDashboard />
+    </div>
+);
+
+export default App;
+        
